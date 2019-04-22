d20c7945350a795b35a1a0b5fd7a2a878e634857
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-04-22 20:12:39 -0300

    Adds Extended Euclidean Algorithm

---
+++ EEA/algorithm.py
@@ -0,0 +1,45 @@
+import os
+
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print("Digite o primeiro número: ")
+    n1 = input()
+
+    print("Digite o segundo número: ")
+    n2 = input()
+
+    print()
+    print()
+    print()
+    r1, r2, r3 = extented_euclides_algorithm(int(n1), int(n2))
+    print()
+    print()
+    print()
+
+
+    n1, n2, r1, r2, r3 = str(n1), str(n2), str(r1), str(r2), str(r3)
+    print("MDC(" + n1 + "," + n2 + ") = (" + r1 + ")(" + n1 + ") + (" + r2 + ")(" + n2 + ") = " +  r3)
+
+
+
+
+def extented_euclides_algorithm(first, second):
+    if second == 0:
+        return 1, 0, first
+   
+    else:
+
+        rest = first % second
+
+        x , y, mdc = extented_euclides_algorithm(second, rest)
+
+        quociente = first//second
+
+        return y, x-(quociente)*y, mdc
+
+
+
+
+main()
