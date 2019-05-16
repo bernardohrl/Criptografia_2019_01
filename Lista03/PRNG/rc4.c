839f14b88c7275a696b8a04db76d7af5e25981c6
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-05-16 13:56:48 -0300

    Finishes RC4

--- Lista03/PRNG/rc4.c
+++ Lista03/PRNG/rc4.c
@@ -9,7 +9,6 @@ void KSA (unsigned char state[], unsigned char key[], int key_size){
         state[i] = i;
     }
 
-    // Shuffling
     for (i=0; i < 256; ++i) {
         j = (j + state[i] + key[i % key_size]) % 256;
         temp = state[i];
@@ -19,9 +18,30 @@ void KSA (unsigned char state[], unsigned char key[], int key_size){
 }
 
 
+void PRNG(unsigned char state[],int qtd){
+    int i=0, j=0, temp=0, k=0;
+    unsigned char character;
+
+    FILE *outF = fopen("output.txt", "w");
+
+    for (k=0; k < qtd; k++)  {
+        i = (i + 1) % 256;
+        j = (j + state[i]) % 256;
+        temp = state[i];
+        state[i] = state[j];
+        state[j] = temp;
+
+        character = state[(state[i] + state[j]) % 256];
+        fputc(character, outF);
+    }
+
+    fclose(outF);
+}
+
 int main(){
     unsigned char key[256];
     unsigned char state[256];
+    int qtd=0;
 
 
     printf("Insira uma chave de até 256 bits: ");
@@ -31,7 +51,11 @@ int main(){
 
     KSA(state, key, key_size);
 
-    printf("\n\n%s\n\n", state);
+
+    printf("Insira a quantidade de bits que deseja gerar: " );
+    scanf("%d", &qtd);
+
+    PRNG(state, qtd);
 
     return 0;
 }
